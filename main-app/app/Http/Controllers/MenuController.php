dd122966ad7d74ff9c3c19a877eb7a0d56cbd225
avicenan <avicena.n@gmail.com>
2024-05-28 22:40:35 +0700

    fix: perubahan service restaurant

--- main-app/app/Http/Controllers/MenuController.php
+++ main-app/app/Http/Controllers/MenuController.php
@@ -9,11 +9,15 @@ use App\Models\Menu;
 class MenuController extends Controller
 {
     public function menuByResto(string $id) {
-        $resto = Restaurant::getRestaurant($id);
+        $data = Restaurant::getRestaurant($id);
+        $resto = $data['resto'];
+        $media = $data['media'];
+
         $menu = Menu::getMenu($id);
         
         return view('restaurant.show-menu', [
             'resto' => $resto,
+            'restoMedia' => $media,
             'menus' => $menu
         ]);
     }
