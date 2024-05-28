596b3a48314c3052ba7c97f17c26344ab6a7394c
avicenan <avicena.n@gmail.com>
2024-05-28 11:08:41 +0700

    feat: menambahkan service restaurant

--- main-app/app/Http/Controllers/RestaurantController.php
+++ main-app/app/Http/Controllers/RestaurantController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class RestaurantController extends Controller
 {
+    protected $getAllRestoURL = 'https://tubeseai-restaurant.hasura.app/api/rest/get-all-restaurant';
+
     public function index()
-    {
-        
+    { 
+        $data = Restaurant::getAllRestaurant();
+        $carouselData = $data->sortByDesc('id')->take(3);
+        $topRatedData = $data->take(3);
+
+        // dd($carouselData);
+        // arsort($carouselData);
+        // $carouselData = array_slice($carouselData, 0, 3);
+
+        return view('restaurant.index', [
+            'restaurantData' => $data,
+            'carouselResto' => $carouselData,
+            'topRatedResto' => $topRatedData
+        ]);
     }
 }
---
+++ main-app/app/Models/Restaurant.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    public static function getAllRestaurant()
+    {
+        try {
+
+            $response = Http::withHeaders([
+                'x-hasura-admin-secret' => 'OYD5qBvKTzW6C3UH1luLce5kwwSxnknMntTBh1zY2oRrbvXhhvnuSlKQJe7DUzMT'
+            ])->get('https://tubeseai-restaurant.hasura.app/api/rest/get-all-restaurant');
+
+            $data = $response->json('restaurant_db_restaurant');
+            $data = Restaurant::hydrate($data);
+            $data = $data->flatten();
+
+            return $data;
+
+        } catch (\Exception $e) {
+            // Handle Error
+            return dd($e);
+        }
+    }
+}
